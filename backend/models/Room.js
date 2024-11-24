173b53e9e008233303cafe0cf6d8ef55baa49b9d
wankhairul11 <w.k.aiman1993@gmail.com>
2024-11-24 21:28:55 +0800

    change position

--- backend/models/Room.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const roomSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    players: { type: Array, default: [] },
-});
-
-module.exports = mongoose.model('Room', roomSchema);
